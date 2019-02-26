bd36ee97208a3539faf9f72b017ad208288d4ba1
e.v.e.r.e.s.tt.1551 <e.v.e.r.e.s.tt.1551@gmail.com>
2019-02-26 17:08:40 +0300

    uaer auth

--- WorkFlow/Controllers/ValuesController.cs
+++ WorkFlow/Controllers/ValuesController.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WorkFlow.Models;
 
 namespace WorkFlow.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private Context db;
+
+        public ValuesController(Context context)
+        {
+            db = context;
+        }
+
         [Authorize]
         [Route("getlogin")]
         public IActionResult GetLogin()
@@ -17,23 +25,23 @@ namespace WorkFlow.Controllers
             return Ok($"Ваш логин: {User.Identity.Name}");
         }
 
-        [Authorize(Roles = "Admin, User")]
+        [Authorize(Roles = "Admin, User, Manager")]
         [Route("getrole")]
         public IActionResult GetRole()
         {
-            if (User.IsInRole("Admin"))
+            string result = "";
+            var roleNames = db.Roles.Select(x => x.Name);
+
+            foreach (string name in roleNames)
             {
-                return Ok("Ваша роль: администратор");
+                if (User.IsInRole(name))
+                {
+                    result += " " + name;
+                }
             }
-            else if(User.IsInRole("User"))
-            {
-                return Ok("Ваша роль: пользователь");
-            }
-            else
-            {
-                return Ok("Ваша роль: can`t found");
-            }         
-           
+
+            return Ok(result);
+
         }
 
     }
